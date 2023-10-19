01edb5683f809ae47365dcc1726915518ad747ae
TomasSchugurensky <t.schugurensky@alumno.um.edu.ar>
2023-10-19 10:42:27 -0300

    eight-twentieth commit

--- game/scrabblegame.py
+++ game/scrabblegame.py
@@ -34,8 +34,23 @@ class ScrabbleGame:
             if self.turn == 0:  
                 self.current_players = self.players
 
-    def calculate_word_value(self):
-        pass
+    def calculate_words_value(self, word):
+        letter_values = {
+            'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4,
+            'I': 1, 'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'Ñ': 8, 'O': 1, 'P': 3,
+            'Q': 5, 'R': 1, 'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 8, 'X': 8,
+            'Y': 4, 'Z': 10, 'CH': 5, 'LL': 8, 'RR': 8, '': 0 
+        }
+        word = word.upper()
+        total_value = 0
+
+        for letter in word:
+            if letter in letter_values:
+                total_value += letter_values[letter]
+            else:
+                total_value += 0
+        return total_value
+
 
     def get_words_input(self):
         pass
